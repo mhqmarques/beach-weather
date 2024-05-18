a3cfd5532f5a91a3345964e9a220bd9ee33f3da1
Marlon Henrique Queroga Marques <mhqmarques@gmail.com>
2024-05-17 22:49:47 -0300

    fix: fixes condition to dbName on connection

--- globals.ts
+++ globals.ts
@@ -1,3 +1,4 @@
 // eslint-disable-next-line
 var testRequest: import('supertest').Agent;
 var nodeEnv = process.env.NODE_ENV;
+var mongoUrlEnv = process.env.MONGODB_URL;
--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,7 @@ import { connect as mongooseConnect, connection } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
-const opt =
-  globalThis.nodeEnv === 'production' ? { dbName: 'surf-forecast' } : {};
+const opt = globalThis.mongoUrlEnv ? { dbName: 'surf-forecast' } : {};
 
 export const connect = async (): Promise<void> => {
   await mongooseConnect(dbConfig.get('mongoUrl'), opt);
