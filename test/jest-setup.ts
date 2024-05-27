c67421371f5f8eba79b3b276384d48ad7f024a58
Marlon Henrique Queroga Marques <mhqmarques@gmail.com>
2024-05-27 11:22:50 -0300

    fix: add port parameter when initing server in test

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,10 +1,11 @@
 import { SetupServer } from '@src/server';
+import config from 'config';
 import supertest from 'supertest';
 
 let server: SetupServer;
 
 beforeAll(async () => {
-  server = new SetupServer();
+  server = new SetupServer(config.get('App.port'));
   await server.initServer();
   globalThis.testRequest = supertest(server.getApp());
 });
